8b1c740762dc94921078e6af88c4f8fadbe2294c
pythonportal <pythonportal@noreply.com>
2024-02-01 20:31:22 +0000

    Add Terraform files

---
+++ locals.tf
@@ -0,0 +1,22 @@
+
+    locals{
+        aci_fabric_node_member_iterations = csvdecode(file("${path.module}/data/aci_fabric_node_member.csv"))
+        
+        aci_fabric_node_member_rows = {
+            for i in local.aci_fabric_node_member_iterations : i.NODE_ID => {
+                FABRIC_NAME     = i.FABRIC_NAME
+                NODE_ROLE       = i.NODE_ROLE
+                POD_ID          = i.POD_ID
+                NODE_ID         = i.NODE_ID  
+                NODE_PEER_ID    = i.NODE_PEER_ID
+                SERIAL_NUMBER   = i.SERIAL_NUMBER
+                NODE_NAME       = i.NODE_NAME
+            }
+        }
+        
+        filtered_node_role_leaf_rows = {
+            for key, value in local.aci_fabric_node_member_rows : key => value
+            if value.NODE_ROLE == "leaf"
+        }
+    }
+    
